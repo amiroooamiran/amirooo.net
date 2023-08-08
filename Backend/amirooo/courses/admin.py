3fd11f517d88cbd643b191397aebebc26bc6d2e4
AmirhosseinAmiran <amiran.amirhossein@gmail.com>
2023-08-07 23:27:56 -0700

    adding models of courses in admin pannel

--- Backend/amirooo/courses/admin.py
+++ Backend/amirooo/courses/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
-
+from courses.models import *
 # Register your models here.
+
+class TagAdmin(admin.TabularInline):
+    model = Tag
+
+class LearnAdmin(admin.TabularInline):
+    model = Learning
+
+class PrerequisiteAdmin(admin.TabularInline):
+    model = Prerequisite
+
+# addin all TabularInline Models in Course Models.
+
+class CourseAdmin(admin.ModelAdmin):
+    inlines = [TagAdmin, LearnAdmin, PrerequisiteAdmin]
+
+admin.site.register(Course, CourseAdmin)
