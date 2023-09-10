842f09ceefdc91907066809a0df5529256d59ed9
AmirhosseinAmiran <amiran.amirhossein@gmail.com>
2023-09-09 17:14:15 -0700

    create urls and views of user app

--- Backend/amirooo/user/views.py
+++ Backend/amirooo/user/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
+def singInSingUp(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        print(username, password)
+    
+    return render(request, 'user/SingInSingUp.html')
