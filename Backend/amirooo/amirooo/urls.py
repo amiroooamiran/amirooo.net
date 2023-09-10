763b599c91ac11187fe3d5db5ae5e9a06b0a94bd
AmirhosseinAmiran <amiran.amirhossein@gmail.com>
2023-09-09 22:56:32 -0700

    add urls of courses

--- Backend/amirooo/amirooo/urls.py
+++ Backend/amirooo/amirooo/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')),
     path('LoginSingup/', include('user.urls')),
+    path('Course', include('courses.urls'))
 ]
