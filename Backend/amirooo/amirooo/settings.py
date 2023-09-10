4fb6a31d08366469902858118b7afe8b43439c07
AmirhosseinAmiran <amiran.amirhossein@gmail.com>
2023-09-09 17:40:28 -0700

    adding Static fiels in Settings of Project

--- Backend/amirooo/amirooo/settings.py
+++ Backend/amirooo/amirooo/settings.py
@@ -128,6 +128,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+
 MEDIA_URL = '/media/'  # or any prefix you choose
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage' # connecting my project to the clud
