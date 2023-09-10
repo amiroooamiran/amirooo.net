a12ee9b0301e99d7c42c8e9dd2bcbbaa08bb5019
AmirhosseinAmiran <amiran.amirhossein@gmail.com>
2023-09-10 02:55:47 -0700

    Create views of MainCourses

--- Backend/amirooo/courses/views.py
+++ Backend/amirooo/courses/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
-
+from courses.models import *
 # Create your views here.
+
+def MainCourses(request):
+    courses = Course.objects.all()
+    tags = Tag.objects.all()
+
+    context ={
+        'courses' : courses,
+        'tags' : tags
+    }
+    return render(request, 'course/Index_course.html', context)
