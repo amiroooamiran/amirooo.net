64a90b3c0d20b62468a300275ce8e39867a15f64
AmirhosseinAmiran <amiran.amirhossein@gmail.com>
2023-09-10 03:11:42 -0700

    creating star and like and resorse of project

--- Backend/amirooo/courses/models.py
+++ Backend/amirooo/courses/models.py
@@ -1,15 +1,19 @@
 from django.db import models
 from tinymce.models import HTMLField
+from user.models import User
 # Create your models here.
 class Course(models.Model):
-    title = models.CharField(max_length= 30, null=True)
+    teacher = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    title = models.CharField(max_length= 150, null=True)
+    banner = models.ImageField(upload_to='course/', blank=True)
     description = HTMLField(null=True)
     price = models.IntegerField(default=0.0, null=False)
     discount = models.IntegerField(null=False, default=0)
     active = models.BooleanField(default=False)
     date = models.DateTimeField(auto_now_add=True)
-    resource = models.FileField(upload_to="course/resource")
     length = models.IntegerField(null=False)
+    likes = models.ManyToManyField(User, related_name='Likes')
+    Star = models.ManyToManyField(User, related_name='Star')
 
     def __str__(self):
         return self.title
@@ -33,6 +37,7 @@ class Video(models.Model):
     title = models.CharField(max_length=100, null=False)
     course = models.ForeignKey(Course, null=False, on_delete=models.CASCADE)
     serial_number = models.IntegerField()
+    resource = models.FileField(upload_to="course/resource", blank=True)
     video_file = models.FileField(upload_to="course/videos")
     is_prwview = models.BooleanField(default=False)
     
