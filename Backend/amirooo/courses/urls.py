76cd92146163db6006de278d2b902f9c624353af
AmirhosseinAmiran <amiran.amirhossein@gmail.com>
2023-09-10 02:56:53 -0700

    creating all of urls for MainCourses

--- Backend/amirooo/courses/urls.py
+++ Backend/amirooo/courses/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
+from courses.views import *
 
 urlpatterns = [
     path('tinymce/', include('tinymce.urls')),
+    path('', MainCourses, name='MainCourses')
 ]
