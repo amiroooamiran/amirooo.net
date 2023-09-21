16481ff8dcc05220863eef87a103488951a829ec
AmirhosseinAmiran <amiran.amirhossein@gmail.com>
2023-09-22 01:56:18 +0330

    tinymc for user

--- Backend/amirooo/user/urls.py
+++ Backend/amirooo/user/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from user.views import *
 
 urlpatterns = [
+    path('tinymce/', include('tinymce.urls')),
     path('', singInSingUp, name='loginForm')
 ]
