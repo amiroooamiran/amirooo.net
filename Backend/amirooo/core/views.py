71799eb420fc4be08ce7a5c5539c0f361eb3ed09
AmirhosseinAmiran <amiran.amirhossein@gmail.com>
2023-09-24 17:20:32 -0700

    create user autenti catin in core

--- Backend/amirooo/core/views.py
+++ Backend/amirooo/core/views.py
@@ -1,9 +1,21 @@
 from django.shortcuts import render
-
+from user.models import User as us
 # Create your views here.
 
 def Index(request):
-    return render(request, 'Index/Index.html')
+
+    if request.user.is_authenticated:
+        # Get a single user profile object that matches the logged-in user
+        user_profile = us.objects.get(user=request.user)
+    else:
+        # Do something else for anonymous users, such as showing a default profile or a message
+        user_profile = None
+
+    context = {
+        'user_profile' : user_profile
+    }
+    return render(request, 'Index/Index.html', context)
+
 
 def about(request):
     return render(request, 'Index/about.html')
