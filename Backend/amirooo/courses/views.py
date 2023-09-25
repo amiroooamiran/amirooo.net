ecf5a5d73de39d7567fa28c50a1ef89fdc3c9de3
AmirhosseinAmiran <amiran.amirhossein@gmail.com>
2023-09-24 17:20:07 -0700

    Create User autenti cation in views of course

--- Backend/amirooo/courses/views.py
+++ Backend/amirooo/courses/views.py
@@ -1,14 +1,23 @@
 from django.shortcuts import render
 from courses.models import *
+from user.models import User as us
 # Create your views here.
 
 def MainCourses(request):
     courses = Course.objects.all()
     tags = Tag.objects.all()
 
+    if request.user.is_authenticated:
+        # Get a single user profile object that matches the logged-in user
+        user_profile = us.objects.get(user=request.user)
+    else:
+        # Do something else for anonymous users, such as showing a default profile or a message
+        user_profile = None
+
     context ={
         'courses' : courses,
-        'tags' : tags
+        'tags' : tags,
+        'user_profile' : user_profile
     }
     return render(request, 'course/MainCourse.html', context)
 
