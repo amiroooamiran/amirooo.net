1a80806de0e1ccc333a07bfab502de1d37d9aceb
AmirhosseinAmiran <amiran.amirhossein@gmail.com>
2023-09-26 00:30:23 -0700

    create views for singIn or singUp

--- Backend/amirooo/user/views.py
+++ Backend/amirooo/user/views.py
@@ -4,8 +4,6 @@ from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
 
-from django.db import IntegrityError
-
 import re
 
 from user.models import User as Us
@@ -27,37 +25,30 @@ def singInSingUp(request):
         
         else:
             try:
-                pattern = "^.*(?=.{6,})(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).*$"
-                result = re.findall(pattern, password_up)
 
-                if (result):
-                    if User.objects.filter(username=username_up).exists():
-                        messages.info(request, ".نام کاربری قبلا ثبت شده")
-                        return redirect('loginForm')
-                    elif User.objects.filter(email=email).exists():
-                        messages.info(request, ".این ایمل قبلا یافت شده")
-                        return redirect('loginForm')
-                    else:
-                        user = User.objects.create_user(username_up, email, password_up)
-                        user.save()
-
-                        our_user = authenticate(username=username_up, password=password_up)
-
-                        if our_user is not None:
-                            login(request, user)
-                            new_profile = Us.objects.create(user=user)
-                            new_profile.save()
-                            return redirect('/')
-                        
-                        messages.success(request, ".ثبت نام با موفقیت انجام شد")
+                if User.objects.filter(username=username_up).exists():
+                    messages.info(request, ".نام کاربری قبلا ثبت شده")
+                    return redirect('loginForm')
+                elif User.objects.filter(email=email).exists():
+                    messages.info(request, ".این ایمل قبلا یافت شده")
+                    return redirect('loginForm')
                 else:
-                    messages.info(request, ".رمز عبور باید دارای 8 کارکتر و متشکل از حروف کوشک و بزرگ و یک علامت باشد")
-                    return redirect('/')
+                    user = User.objects.create_user(username_up, email, password_up)
+                    user.save()
 
-                login(request, user) # log in the new user
-                return redirect('/')
-            except IntegrityError:
-                messages.info(request, ".نام کاربری یا رمز عبور اشتباه است")
+                    our_user = authenticate(username=username_up, password=password_up)
+
+                    if our_user is not None:
+                        login(request, user)
+                        new_profile = Us.objects.create(user=user)
+                        new_profile.save()
+                        messages.success(request, ".ثبت نام با موفقیت انجام شد")
+                        return redirect('/')
+                       
+                    return redirect('loginForm')
+            except:
+                if user is None:
+                    messages.info(request, ".نام کاربری یا رمز عبور اشتباه است")
     
         
     return render(request, 'user/SingInSingUp.html')
