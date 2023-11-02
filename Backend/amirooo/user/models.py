c98be4e75a247ab5923f854ba83bb3a8cb9c647a
AmirhosseinAmiran <amiran.amirhossein@gmail.com>
2023-11-02 18:43:24 +0330

    create course of hte user

--- Backend/amirooo/user/models.py
+++ Backend/amirooo/user/models.py
@@ -3,6 +3,8 @@ from django.contrib.auth.models import User
 from django.core.validators import RegexValidator
 from tinymce.models import HTMLField
 
+from courses.models import Course
+
 # Create your models here.
 class User (models.Model):
     Profile = models.ImageField(upload_to="profiles", default="defultProfile.jpg")
@@ -13,6 +15,7 @@ class User (models.Model):
     phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$', message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")
     Phone_number = models.CharField(validators=[phone_regex], max_length=17, blank=True) # Validators should be a list
     Email = models.EmailField(max_length=254, blank=True)
+    User_course = models.ManyToManyField(Course, blank=True)
 
     def __str__(self):
         return self.user.username
