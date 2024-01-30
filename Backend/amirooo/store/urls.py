cf1ab366a34fd9b67d24313d16080548443384d6
AmirhosseinAmiran <amiran.amirhossein@gmail.com>
2024-01-30 10:14:52 +0330

    add tinymc to the url

--- Backend/amirooo/store/urls.py
+++ Backend/amirooo/store/urls.py
@@ -1,7 +1,8 @@
-from django.urls import path
+from django.urls import path, include
 from .views import *
 
 urlpatterns = [
+    path('tinymce/', include('tinymce.urls')),
     path('', store, name="store"),
     path('product/', product, name="product")    
 ]
