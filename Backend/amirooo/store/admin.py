5da513a1dd74fd58df74e50df3788c734fd916ae
amiroooamiran <amiran.amirhossein@gmail.com>
2024-01-31 14:05:36 +0330

    config admin of the store

--- Backend/amirooo/store/admin.py
+++ Backend/amirooo/store/admin.py
@@ -2,10 +2,10 @@ from django.contrib import admin
 from .models import *
 # Register your models here.
 
-class ProductImageInline(admin.TabularInline):  # یا StackedInline بر اساس نیاز
+class ProductImageInline(admin.TabularInline):
     model = ProductImage
 
-class ProductTypeInline(admin.TabularInline):  # یا StackedInline بر اساس نیاز
+class ProductTypeInline(admin.TabularInline): 
     model = ProductType
 
 class ProductAdmin(admin.ModelAdmin):
