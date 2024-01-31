1cde9fb1a5c4c2366c29d10303832439c1ac3bfc
amiroooamiran <amiran.amirhossein@gmail.com>
2024-01-31 14:05:09 +0330

    add models in store views

--- Backend/amirooo/store/views.py
+++ Backend/amirooo/store/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
-
+from .models import *
 # Create your views here.
 
 def store(request):
-    return render(request, 'store/store.html')
+    products = Product.objects.all()
+    productImages = ProductImage.objects.all()
+
+    context = {
+        products : 'products',
+        productImages : 'productImages',
+    }
+    return render(request, 'store/store.html', context)
 
 def product(request):
     return render(request, 'store/product.html')
